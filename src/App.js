cbcbdda0ecb7080c965f7bda0ac83f7d725136e8
zue <zuleikatesei@gmail.com>
2022-11-28 17:22:25 -0800

    nav bar functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from './pages/Home';
-// import About from './pages/About';
-// import Portfolio from './pages/Portfolio';
-// import Contact from './pages/Contact';
-import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import ContactForm from './pages/ContactForm';
+import OffCanvasNav from './components/Navbar';
 
 function App() {
   return (
     <div className='App'>
       <Router>
-        <Navbar />
+        <OffCanvasNav />
         <Routes>
           {/* <Route path='/' element={<Home />} />
           <Route path='about' element={<About />} />
           <Route path='/portfolio' element={<Portfolio />} />
-          <Route path='/contact' element={<Contact />} /> */}
+          <Route path='/contact' element={<ContactForm />} /> */}
         </Routes>
       </Router>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import '../styles/Navbar.css';
+import ReorderIcon from '@material-ui/icons/Reorder';
 
 function Navbar() {
+    const [expandNavBar, setExpandNavBar] = useState(false);
+
+    // close nav bar at location change
+    const location = useLocation();
+    useEffect(() => {
+        setExpandNavBar(false)
+    }, ([location]));
+
     return (
-        <div className='navbar'>
+        <div className='navbar' id={expandNavBar ? 'open' : 'close'}>
             <div className='toggleButton'>
-                <button></button>
+                <button onClick={() => { setExpandNavBar((prev) => !prev); }}> < ReorderIcon /> </button>
             </div>
             <div className='links'>
-                <Link to="/"> Home </Link>
-                <Link to="/about"> About </Link>
-                <Link to="/portfolio"> Portfolio </Link>
-                <Link to="/contact"> Contact Me </Link>
+                <Link to='/'> Home </Link>
+                <Link to='/about'> About </Link>
+                <Link to='/portfolio'> Portfolio </Link>
+                <Link to='/contact'> Contact Me </Link>
             </div>
         </div>
     );

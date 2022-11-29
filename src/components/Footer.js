59876fb01f1577cec5b5645fe1437342eded8b55
zue <zuleikatesei@gmail.com>
2022-11-28 17:30:04 -0800

    set up basic footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,22 @@
 import React from 'react';
+import InstaIcon from '@material-ui/icons/Instagram';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GithubIcon from '@material-ui/icons/GitHub';
+import '../styles/Footer.css';
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <div className='socialMedia'>
+                <InstaIcon />
+                <TwitterIcon />
+                <LinkedInIcon />
+                <GithubIcon />
+            </div>
+            <p> &copy; 2022 lovezue.com </p>
+        </div>
+    );
+}
+
+export default Footer;

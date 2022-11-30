0e801094d7ad6e9abc0ddffe0abc254dce72e1ca
zue <zuleikatesei@gmail.com>
2022-11-29 17:17:01 -0800

    contact form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
-// import About from './pages/About';
-// import Portfolio from './pages/Portfolio';
-// import ContactForm from './pages/ContactForm';
+import About from './pages/About';
+import ContactForm from './pages/ContactForm';
+// import Projects from './pages/Projects';
+// import ProjectDisplay from './pages/ProjectDisplay';
+
 import Nav from './components/Navbar';
 import Footer from './components/Footer';
+
 
 function App() {
   return (
@@ -14,9 +17,10 @@ function App() {
         <Nav />
         <Routes>
           <Route path='/' element={<Home />} />
-          {/* <Route path='about' element={<About />} />
-          <Route path='/portfolio' element={<Portfolio />} />
-          <Route path='/contact' element={<ContactForm />} /> */}
+          {/* <Route path='/projects' element={<Projects />} /> */}
+          {/* <Route path='/projects/:id' element={<ProjectDisplay />} /> */}
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<ContactForm />} />
         </Routes>
         < Footer />
       </Router>

e2ffaa8c9fc18921cf3222392bcaeabfd1a05af6
zue <zuleikatesei@gmail.com>
2022-12-03 18:18:03 -0800

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,12 @@ import EmailIcon from '@material-ui/icons/Email';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GithubIcon from '@material-ui/icons/GitHub';
 import Typed from 'react-typed';
-import Photo from '../assets/home.png';
 import '../styles/Home.css'
 
 function Home() {
     return (
         <div className='Home'>
             <div className='about'>
-                <img src={Photo} />
                 <div>
                     <h2> Zuleika Tesei </h2>
                     <h3> Software Engineer </h3>

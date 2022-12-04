4c290dba1d1477f248f5fd41fb3b246d03bcc555
zue <zuleikatesei@gmail.com>
2022-12-03 18:18:03 -0800

    home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import '../styles/Navbar.css';
 import ReorderIcon from '@material-ui/icons/Reorder';
+import Nav from 'react-bootstrap/Nav';
 
 function Navbar() {
     const [expandNavBar, setExpandNavBar] = useState(false);
@@ -13,17 +14,23 @@ function Navbar() {
     }, ([location]));
 
     return (
-        <div className='navbar' id={expandNavBar ? 'open' : 'close'}>
-            <div className='toggleButton'>
-                <button onClick={() => { setExpandNavBar((prev) => !prev); }}> < ReorderIcon /> </button>
+
+        <Nav justify variant='tabs' defaultActiveKey="/">
+
+
+            <div className='navbar' id={expandNavBar ? 'open' : 'close'}>
+                <div className='toggleButton'>
+                    <button onClick={() => { setExpandNavBar((prev) => !prev); }}> < ReorderIcon /> </button>
+                </div>
+                <div className='links'>
+                    <Link to='/'> Home </Link>
+                    <Link to='/projects'> Projects </Link>
+                    <Link to='/about'> About </Link>
+                    <Link to='/contact'> Contact </Link>
+                </div>
             </div>
-            <div className='links'>
-                <Link to='/'> Home </Link>
-                {/* <Link to='/projects'> Projects </Link> */}
-                <Link to='/about'> About </Link>
-                <Link to='/contact'> Contact </Link>
-            </div>
-        </div>
+
+        </Nav>
     );
 }
 

1878f6939385295524de44644ec3bb0bcabae5f5
zue <zuleikatesei@gmail.com>
2022-12-07 12:09:09 -0800

    overhauled home page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,20 +1,7 @@
 import React from 'react';
-import ProjectItem from '../components/ProjectItem'
-import { ProjectList } from '../helpers/ProjectList';
 
-import '../styles/Projects.css'
+function ProjectDisplay() {
 
-function Projects() {
-    return (
-        <div className='projects'>
-            <h1> My Projects </h1>
-            <div className='projectList'>
-                {ProjectList.map((project) => {
-                    return <ProjectItem name={project.name} image={project.image} />;
-                })}
-            </div>
-        </div>
-    )
 }
 
-export default Projects;
+export default ProjectDisplay;

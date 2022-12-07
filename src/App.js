09c806eedfddc35ee466493eebd2f2ba0cbbd3bc
zue <zuleikatesei@gmail.com>
2022-12-07 12:28:51 -0800

    using mycard as nav

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import About from './pages/About';
-import ContactForm from './pages/ContactForm';
-// import Projects from './pages/Projects';
+import Connect from './pages/Connect';
+import Projects from './pages/Projects';
 // import ProjectDisplay from './pages/ProjectDisplay';
-
-import Nav from './components/Navbar';
+// import Nav from './components/Navbar';
 // import Footer from './components/Footer';
 
 
@@ -14,13 +13,12 @@ function App() {
   return (
     <div className='App'>
       <Router>
-        <Nav />
+        {/* <Nav /> */}
         <Routes>
           <Route path='/' element={<Home />} />
-          {/* <Route path='/projects' element={<Projects />} /> */}
-          {/* <Route path='/projects/:id' element={<ProjectDisplay />} /> */}
+          <Route path='/projects' element={<Projects />} />
           <Route path='/about' element={<About />} />
-          <Route path='/contact' element={<ContactForm />} />
+          <Route path='/contact' element={<Connect />} />
         </Routes>
         {/* < Footer /> */}
       </Router>
--- src/pages/Connect.js
+++ src/pages/Connect.js
@@ -3,11 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ContactModal from '../components/ContactModal';
-import InstaIcon from '@material-ui/icons/Instagram';
-import TwitterIcon from '@material-ui/icons/Twitter';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import GithubIcon from '@material-ui/icons/GitHub';
-import PinterestIcon from '@material-ui/icons/Pinterest';
+import MyCard from '../components/Card';
 
 import '../styles/Contact.css';
 
@@ -17,22 +13,17 @@ function ContactForm() {
         <Container className="contact">
             <Row>
                 <Col xs={12} md={8}>
+                    <MyCard />
+                </Col>
+                <Col xs={6} md={4}>
                     <h2> Contact. </h2>
                     <div className='contact-info'>
                         <p> T: +1 (916) 479-6064 <br></br>
                             E: zuleikatesei@gmail.com
                         </p>
                         <p> Schedule a <a href='https://calendly.com/zuetesei/30min' target='_blank' rel="noreferrer">chat</a>!  </p>
-                        <p> Follow me here: </p>
                     </div>
-                    <a href='https://www.instagram.com/love.zue/' target="_blank" rel="noreferrer"> <InstaIcon /> </a>
-                    <a href='https://www.pinterest.com/internationalzue/' > <PinterestIcon /> </a>
-                    <a href='https://twitter.com/thenationalzue' > <TwitterIcon /> </a>
-                    <a href='https://www.linkedin.com/in/zuleika-tesei/' target="_blank" rel="noreferrer"> <LinkedInIcon /> </a>
-                    <a href='https://github.com/zuetesei' target="_blank" rel="noreferrer"> <GithubIcon /> </a>
 
-                </Col>
-                <Col xs={6} md={4}>
                     <h3> Leave a Note. </h3>
                     <div className="modalBtn">
                         <ContactModal />

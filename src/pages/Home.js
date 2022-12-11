238da0264fb2f77c199c9f8617f7b325e1aca0c4
zue <zuleikatesei@gmail.com>
2022-12-11 00:58:58 -0800

    hard code projects

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,13 +4,14 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import MyCard from '../components/Card';
 import '../styles/Home.css';
+// import OceanGif from '../assets/ocean.gif';
 
 function Home() {
     return (
         <Container className='home'>
             <Row>
                 <Col xs={6} md={4}> <MyCard /> </Col>
-                <Col xs={12} md={8}> What should go here? </Col>
+                <Col xs={12} md={8}>  </Col>
             </Row>
         </Container>
 
